25acc21656a7694afbf795059a0365592f848707
Ammar Chishti <ammarchishti80@gmail.com>
2023-06-06 01:01:54 -0400

    Initial commit of XCode boilerplate code

---
+++ JPMorgan_Coding_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPMorgan_Coding_ExerciseApp.swift
+//  JPMorgan-Coding-Exercise
+//
+//  Created by Ammar Chishti on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct JPMorgan_Coding_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
